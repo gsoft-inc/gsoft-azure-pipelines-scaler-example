abfbc31c8a612c47b434ea5892630d354eafa0ec
Yohan Belval <yohan.belval@gmail.com>
2022-01-31 11:12:49 -0500

    Update start.sh

--- 01-image/start.sh
+++ 01-image/start.sh
@@ -91,4 +91,4 @@ chmod +x ./run-docker.sh
 
 # To be aware of TERM and INT signals call run.sh
 # Running it with the --once flag at the end will shut down the agent after the build is executed
-./run.sh "$@" & wait $!
+./run-docker.sh "$@" & wait $!
